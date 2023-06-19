f6a859dd7e11dd5e74dbd601bf7eaaa193b63d0f
diegoinost02 <diegoinostroza00@gmail.com>
2023-06-19 18:57:18 -0300

    Cree vistas USUARIO_Seleccion y USUARIO_Venta, con sus respectivos controladores

---
+++ src/main/java/com/tp/tp_final_lab3/controllers/usuarioVentaController.java
@@ -0,0 +1,53 @@
+package com.tp.tp_final_lab3.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class usuarioVentaController {
+    @FXML
+    private Text text;
+    @FXML
+    private Button verificarButton;
+    @FXML
+    private Button crearButton;
+    @FXML
+    private Button salirButton;
+
+    public void verificar(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/tp/tp_final_lab3/Views/verificar")); //poner ruta de verificar
+            Stage stage = (Stage) verificarButton.getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+        }catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+    public void crear(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/tp/tp_final_lab3/Views/crear")); //poner ruta de crear cliente
+            Stage stage = (Stage) crearButton.getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+    public void salir(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/tp/tp_final_lab3/Views/LOGIN_Importadora.fxml"));
+            Stage stage = (Stage) salirButton.getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+        }catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
