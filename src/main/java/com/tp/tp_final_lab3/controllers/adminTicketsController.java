5e56230542d2bc3fcb2ae0a9181878bd12fa4646
diegoinost02 <diegoinostroza00@gmail.com>
2023-06-22 01:23:44 -0300

    .

---
+++ src/main/java/com/tp/tp_final_lab3/controllers/adminTicketsController.java
@@ -0,0 +1,50 @@
+package com.tp.tp_final_lab3.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class adminTicketsController {
+
+    @FXML
+    private TableColumn<?, ?> columnCantidad;
+
+    @FXML
+    private TableColumn<?, ?> columnCatFiscal;
+
+    @FXML
+    private TableColumn<?, ?> columnDni;
+
+    @FXML
+    private TableColumn<?, ?> columnFecha;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnPrecio;
+
+    @FXML
+    private TableColumn<?, ?> columnProducto;
+
+    @FXML
+    private TableColumn<?, ?> columnVendedor;
+
+    @FXML
+    private Button verTicketButton;
+
+    @FXML
+    private Button volverButton;
+
+    @FXML
+    void verTicket(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volver(ActionEvent event) {
+
+    }
+
+}
